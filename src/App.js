4694b5d65e17d3a04fa947b444707890633d6cc3
Suizhazha <573505403@qq.com>
2020-09-01 22:00:11 +0800

    复习state和props,使用了被弃用的componentWillReceiveProps

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React from 'react';
 
 
-function App() {
-  return (
-    <div className="App">
-      hi
-    </div>
-  );
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {x:1}
+  }
+  onClick = ()=>{
+    this.setState((state)=>({
+      x:state.x+1
+    })
+    )
+  }
+  render() {
+    return(
+      <div className='App'>
+        App
+        <button onClick={this.onClick}>+1</button>
+        <B name={this.state.x}/>
+      </div>
+    )
+  }
+}
+
+class B extends React.Component{
+
+  componentWillReceiveProps(nextProps, nextContext) {
+    console.log(`旧的props:`)
+    console.log(this.props)
+    console.log('props变化了')
+    console.log(nextProps)
+  }
+  constructor() {
+    super();
+  }
+  render() {
+    return(
+      <div>{this.props.name}</div>
+    )
+  }
 }
 
 export default App;
