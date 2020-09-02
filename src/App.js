5b6ebc234a3d55a1b4f9af9b4cbc129e6335b833
Suizhazha <573505403@qq.com>
2020-09-02 20:32:50 +0800

    使用useEffect模拟componentDidMount

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ const App =(props)=>{
   const onClick = ()=>{
     setN(n+1)
   }
+   React.useEffect(()=>{
+     console.log('第一次render')
+   },[])
+
   return (
     <div>{n}
     <button onClick={onClick}>+1</button>
     </div>
   )
-
 }
 
 export default App
+
