1991531dffb925b8c8c1b3e6b182eccad84b2682
Suizhazha <573505403@qq.com>
2020-09-02 11:56:54 +0800

    使用？：表达式

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
-import React from 'react';
+import React from "react"
 
 
-class App extends React.PureComponent{
- constructor(props) {
-   super(props);
-   this.state = {
-     n: 0
-   }
- }
-onClick = () =>{
- this.setState( (state) =>(
-   {
-     n:state.n+1
-   }
- ))
-  }
-  render() {
-   let message
-    if (this.state.n %2){
-      message = <div>偶数</div>
-    }else {
-      message = <div>奇数</div>
+class App extends React.PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      n: 0
     }
+  }
+
+  onClick = () => {
+    this.setState((state) => (
+      {
+        n: state.n + 1
+
+      }
+    ))
+  }
+
+  render() {
     return (
       <>
-        { message }
+        {this.state.n % 2 ? <div>偶数</div> : <div>奇数</div>}
         <button onClick={this.onClick}>+1</button>
-        </>
+      </>
     )
- }
+  }
 }
 
-export default App;
+export default App
