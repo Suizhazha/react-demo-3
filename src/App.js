30347fb26b032887c7dd84c3a343e62f1669b87c
Suizhazha <573505403@qq.com>
2020-09-02 11:44:29 +0800

    使用React.PureComponent,并在render中使用if else 判断奇偶

--- src/App.js
+++ src/App.js
@@ -1,44 +1,34 @@
 import React from 'react';
 
 
-class App extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = {x:1}
-  }
-  onClick = ()=>{
-    this.setState((state)=>({
-      x:state.x+1
-    })
-    )
+class App extends React.PureComponent{
+ constructor(props) {
+   super(props);
+   this.state = {
+     n: 0
+   }
+ }
+onClick = () =>{
+ this.setState( (state) =>(
+   {
+     n:state.n+1
+   }
+ ))
   }
   render() {
-    return(
-      <div className='App'>
-        App
+   let message
+    if (this.state.n %2){
+      message = <div>偶数</div>
+    }else {
+      message = <div>奇数</div>
+    }
+    return (
+      <>
+        { message }
         <button onClick={this.onClick}>+1</button>
-        <B name={this.state.x}/>
-      </div>
+        </>
     )
-  }
-}
-
-class B extends React.Component{
-
-  componentWillReceiveProps(nextProps, nextContext) {
-    console.log(`旧的props:`)
-    console.log(this.props)
-    console.log('props变化了')
-    console.log(nextProps)
-  }
-  constructor() {
-    super();
-  }
-  render() {
-    return(
-      <div>{this.props.name}</div>
-    )
-  }
+ }
 }
 
 export default App;
